de65f5d03d0eba74e220f1646efee3b8159bfa56
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-01 01:19:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 
 class Shoe
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble(condition)
+    @condition = condition
+    condition = "new"
   end
 
 end
